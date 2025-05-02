4df1172300d37d3ef73330293493803f444d96d6
Ashley Egbuji <nkemegbuji@gmail.com>
2025-05-02 11:44:46 +0100

    Enabled bet even or odd functionality

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -4,6 +4,10 @@ let crapsUsername = ""
 // Craps Game Settings
 const startingMoney = 1000
 const startingRounds = 0 
+const bets = {
+    even: "EVEN",
+    odd: "ODD"
+}
 
 // HTML Element IDs
 const crapsUsernameInput = "craps-username-input"
@@ -16,6 +20,7 @@ const crapsStatsRounds = "craps-stats-rounds"
 //In-game variables
 let currentMoney = startingMoney
 let currentRounds = startingRounds
+let currentBet = bets.even
 
 function registerCrapsPlayer () {
     let crapsUsername = document.getElementById(crapsUsernameInput).value
@@ -43,10 +48,23 @@ function setupFirstRound () {
     currentRounds = startingRounds
     setMoney (startingMoney)
     setRounds (currentRounds)
+    betEven()
 }
 function setMoney (money) {
     document.getElementById(crapsStatsMoney).innerHTML = money
 }
 function setRounds (round) {
     document.getElementById(crapsStatsRounds).innerHTML = round
+}
+function betEven () {
+    chooseBet = bets.even
+}
+function betOdd () {
+    chooseBet = bets.odd
+}
+function chooseBet (bet) {
+    currentBet = bet 
+    document.getElementById(crapsRegistrationPane).style.backgroundColor = "red"
+    const deselectBet = bet == bets.even ? bets.odd : bets.even
+    document.getElementById(deselectBet).style.backgroundColor = "transparent"
 }
