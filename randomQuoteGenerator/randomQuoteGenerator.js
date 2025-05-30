b3d23dc64fe3d3f0c68464856fac0b4d3d9d5975
Ashley Egbuji <nkemegbuji@gmail.com>
2025-05-30 11:40:17 +0100

    Completed random quote generator UI

--- randomQuoteGenerator/randomQuoteGenerator.js
+++ randomQuoteGenerator/randomQuoteGenerator.js
@@ -1,3 +1,23 @@
+const randomQuoteGeneratorElement = document.getElementById('random-quote-generator')
+const colors = [
+  [ "#FF8080", "#FFCF96" ],
+  [ "#FFF3DA", "#D0BFFF" ],
+  [ "#8DDFCB", "#82A0DB" ],
+  [ "#F9B572", "#99B080" ],
+  [ "#7C96AB", "#BFCCB5" ],
+  [ "#B7B7B7", "#EDC6B1" ],
+  [ "#FFDEB4", "#FFB4B4" ],
+  [ "#FFAACF", "#B9F3E4" ],
+  [ "#B5F1CC", "#E5FDD1" ],
+  [ "#F7ECDE", "#54BAB9" ],
+  [ "#7897AB", "#D885A3" ],
+  [ "#B97A95", "#F6AE99 "],
+]
+
+function getRandomColorCombo () {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex]
+}
 function getNewRandomQuote () {
   fetch('https://api.quotable.io/quotes/random')  
   .then(response => {
@@ -7,6 +27,8 @@ function getNewRandomQuote () {
     return response.json();
   })
   .then(data => {
+    const colorCombo = getRandomColorCombo()
+    randomQuoteGeneratorElement.style.background = 'linear-gradient(45deg, ' + colorCombo[0] + ', ' + colorCombo[1]+ ')'
     const quoteText = data[0].content
     const quoteAuthor = data[0].content
     document.getElementById("random-quote-text").innerHTML = quoteText
