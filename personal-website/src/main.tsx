8169e075568e027160f9fa2e14f5462c254b5313
Ashley Egbuji <nkemegbuji@gmail.com>
2025-06-11 18:00:44 +0100

    Integrated stock analysis dashboard as react component

--- personal-website/src/main.tsx
+++ personal-website/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import StockAnalysisDashboard from './stockAnalysisDashboard/stockAnalysisDashboard'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StockAnalysisDashboard />
   </StrictMode>,
 )
