536488f7b65bfd13a7461c8ef2902b9e92e2e90f
Ashley Egbuji <nkemegbuji@gmail.com>
2025-06-11 20:58:16 +0100

    Converted analyzeStock workflow to react

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+import { useState, type SetStateAction } from 'react'
+import { analyzeStock } from './stockAnalysisDashboard'
 
 function stockAnalysisDashboard() {
-  const [count, setCount] = useState(0)
+
+ const [stockData, setStockData] = useState()
+ const [stockSymbol, setstockSymbol] = useState('');
+
+  async function runStockAnalysis() {
+    const gotStockData = await analyzeStock (stockSymbol)
+    setStockData(gotStockData)
+  }  
+
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React in stock Dashboard</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
+          <div id="stock-analysis-dashboard-subtitle">
+            Put in a stock symbol you'd like to analyze (e.g. MSFT)
+          </div>
+          <input
+          value={stockSymbol}
+          onChange={e => setstockSymbol(e.target.value)}
+          ></input>
+          <input id="stock-analysis-dashboard-input"></input>
+          <button className="stock-analysis-dashboard-button"onClick={() => runStockAnalysis()}>Analyze</button>
+          <div>
+           {JSON.stringify(stockData)}
+          </div>
       </div>
     </>
   )
