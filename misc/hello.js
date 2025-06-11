05aed16e6ae9d1bd555fab3d2ac1150d3e02177e
Ashley Egbuji <nkemegbuji@gmail.com>
2025-06-11 14:37:16 +0100

    Migrated website to vite / react app

--- misc/hello.js
+++ misc/hello.js
@@ -1,1 +1,26 @@
-console.log("hello from Javascript")
+// let rows = 25
+// console.log('For rows = ' + rows)
+
+// for(let row = 1; row <= rows; row++) {
+//     let printValue = ''
+//     for(let column = 0; column <= row; column++) {
+//     printValue += '*'
+//     }
+//     console.log(printValue)
+// }
+
+let arrays = [ 'arrays', 'are', 'iterables' ]
+let characterCount = {}
+
+console.log(array)
+for (let element of array) {
+    for (let character of element) {
+        if (character in characterCount) {
+        characterCount[character] += 1
+        } else {
+        characterCount[character] = 1
+        }
+    }
+}    
+
+console.log(characterCount)
