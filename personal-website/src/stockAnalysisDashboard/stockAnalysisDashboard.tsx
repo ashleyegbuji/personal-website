240b81c3060a33ccc493d894270a1fe40afa9255
Ashley Egbuji <nkemegbuji@gmail.com>
2025-06-12 13:38:13 +0100

    Implemented conditional rendering on stock data response

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
@@ -1,22 +1,62 @@
 import { useState, type SetStateAction } from 'react'
 import { analyzeStock } from './stockAnalysisDashboard'
+import { Oval } from 'react-loader-spinner'
 
 function stockAnalysisDashboard() {
 
  const [stockData, setStockData] = useState()
  const [stockSymbol, setstockSymbol] = useState('');
+ const [isLoading, setIsLoading] = useState(false)
+ const [gotData, setGotData] = useState(false)
 
-  async function runStockAnalysis() {
+
+ function goBack () {
+      setGotData(true)
+      setIsLoading(false)
+ }
+ async function runStockAnalysis() {
+    setIsLoading(true)
     const gotStockData = await analyzeStock (stockSymbol)
-    setStockData(gotStockData)
-  }  
-
+    if (gotStockData) {
+      setStockData(gotStockData)
+      setGotData(true)
+      setIsLoading(false)
+    } else {
+      goBack()
+    }
+    
+   
+}  
+ if (gotData) {
+    return (
+        <div>
+         <div onClick={() => goBack()}>Back</div>
+          <div>
+           {JSON.stringify(stockData)}
+          </div>
+        </div>
+    )
+ }
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
+         <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
+          {isLoading ? (
+            <div>
+              <Oval
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="oval-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+          </div>
+          ) : (
+            <div>
           <div id="stock-analysis-dashboard-subtitle">
             Put in a stock symbol you'd like to analyze (e.g. MSFT)
           </div>
@@ -26,12 +66,10 @@ function stockAnalysisDashboard() {
           ></input>
           <input id="stock-analysis-dashboard-input"></input>
           <button className="stock-analysis-dashboard-button"onClick={() => runStockAnalysis()}>Analyze</button>
-          <div>
-           {JSON.stringify(stockData)}
           </div>
+           )}
       </div>
     </>
   )
 }
-
 export default stockAnalysisDashboard
